d5b3d512285735a0702664bebba03a5dd659059f
Dora Do <doradocodes@gmail.com>
2024-11-28 02:23:33 -0500

    add live page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/css-cheatsheet/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true, // also necessary
+  }
 })
